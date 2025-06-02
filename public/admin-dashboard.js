416d70d31e8c2e1fea4c1bafc590be198951416c
pang <pang133@example.com>
2025-06-02 17:19:28 +0900

    대시보드 차트 추가

--- public/admin-dashboard.js
+++ public/admin-dashboard.js
@@ -1002,26 +1002,24 @@ async function renderCharts() {
                 return ((parseFloat(prob) / targetValue) * 100).toFixed(2);
             });
 
-            const lineData = {
-                labels: categories,
-                datasets: [{
-                    label: `${targetCategory} 대비 상대 비율`,
-                    data: compareValues,
-                    borderColor: 'rgba(75, 192, 192, 1)',
-                    fill: false,
-                    tension: 0.1
-                }]
-            };
-
             // 이전 꺾은선 그래프 제거
             if (lineChartInstance) {
                 lineChartInstance.destroy();
             }
 
             // 꺾은선 차트 생성
-            lineChartInstance = new Chart(lineCtx, {
+            window.lineChartInstance = new Chart(lineCtx, {
                 type: 'line',
-                data: lineData,
+                data: {
+                    labels: categories,
+                    datasets: [{
+                        label: `${targetCategory} 대비 상대 비율`,
+                        data: compareValues,
+                        borderColor: 'rgba(75, 192, 192, 1)',
+                        fill: false,
+                        tension: 0.1
+                    }]
+                },
                 options: {
                     responsive: true,
                     maintainAspectRatio: false,
@@ -1037,8 +1035,17 @@ async function renderCharts() {
                             beginAtZero: true,
                             ticks: {
                                 callback: value => `${value}%`
+                            },
+                            display: false // 왼쪽 y축은 꺾은선에선 숨김
+                        },
+                        y2:{
+                            beginAtZero: true,
+                            position: 'right',  // 🔥 오른쪽에 축 표시
+                            grid: { drawOnChartArea: false }, // 격자 제거
+                            ticks: {
+                                callback: value => `${value}%`
                             }
-                        }
+                        }    
                     }
                 }
             });
