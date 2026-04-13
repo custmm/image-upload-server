4f4360a40996af1ad95f3cc8b2580d45c1f0c425
pang <pang133@example.com>
2026-04-13 21:33:56 +0900

    관리자모드 수정

--- public/js/admin-dashboard.js
+++ public/js/admin-dashboard.js
@@ -1,3 +1,29 @@
+// 전역 변수 설정 (함수 밖 최상단에 위치)
+let loaderInterval = null;
+let loaderStep = 0;
+
+
+function showLoading() {
+    const indicator = document.getElementById("loadingIndicator");
+    const loader = document.getElementById("mainLoader");
+
+    indicator.style.display = "flex";
+
+    loaderInterval = setInterval(() => {
+        loaderStep++;
+        if (loaderStep > 4) loaderStep = 1;
+
+        loader.className = "loader loader" + loaderStep;
+    }, 150); // 속도 조절 가능
+}
+
+function hideLoading() {
+    clearInterval(loaderInterval);
+    loaderInterval = null;
+
+    document.getElementById("loadingIndicator").style.display = "none";
+}
+
 // 먼저 화면 잠금 (body 존재 보장)
 document.addEventListener("DOMContentLoaded", () => {
     document.body.classList.add("auth-lock");
