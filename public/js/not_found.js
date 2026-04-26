1efd5cbf124db36db537deb5cb0b28c3b5a62382
pang <pang133@example.com>
2026-04-26 22:32:31 +0900

    404페이지 로딩 아이콘 생성

--- public/js/not_found.js
+++ public/js/not_found.js
@@ -21,3 +21,22 @@ function showLoading() {
 document.addEventListener("DOMContentLoaded", () => {
     showLoading(); // 페이지 열리자마자 아이콘이 뜨는지 확인
 });
+
+window.addEventListener('offline', () => {
+    alert("인터넷 연결이 끊겼습니다! 오프라인 모드로 전환합니다.");
+    window.location.href = "./not_found.html";
+});
+
+window.addEventListener('online', () => {
+    // 저장해둔 주소를 꺼냄
+    const lastPath = sessionStorage.getItem('lastPath');
+
+    alert("인터넷이 다시 연결되었습니다!");
+
+    if (lastPath) {
+        window.location.href = lastPath; // 기억해둔 페이지로 복귀
+        sessionStorage.removeItem('lastPath'); // 사용 후 삭제
+    } else {
+        window.location.href = "/index.html"; // 기록 없으면 메인으로
+    }
+});
