fb56e9202d3506533c8ff002a2bb1a4be27e602d
pang <pang133@example.com>
2026-04-27 13:00:04 +0900

    etc페이지 표 링크방식 도입

--- public/js/etc_util.js
+++ public/js/etc_util.js
@@ -9,3 +9,15 @@ document.querySelectorAll(".detail-toggle").forEach(button => {
         detail.classList.toggle("active");
     });
 });
+
+document.querySelectorAll('a[href="#mini_mp3"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault(); // 기본 이동 막기
+        
+        const targetTable = document.getElementById('mini_mp3');
+        targetTable.classList.add('active'); // 표 나타내기
+        
+        // 해당 위치로 부드럽게 이동
+        targetTable.scrollIntoView({ behavior: 'smooth' });
+    });
+});
