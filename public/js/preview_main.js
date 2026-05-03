9aeacf5abec89fcd205658be24360744c1fb1c34
pang <pang133@example.com>
2026-05-03 20:36:39 +0900

    preview페이지 팩맨게임 스타일로 구현

--- public/js/preview_main.js
+++ public/js/preview_main.js
@@ -426,7 +426,7 @@ document.addEventListener("DOMContentLoaded", async () => {
                     const tX = trashRect.left + trashRect.width / 2;
                     const tY = trashRect.top + trashRect.height / 2;
                     const iX = indRect.left + indRect.width / 2;
-                    const iY = indRect.top + iY;
+                    const iY = indRect.top + trashRect.height / 2;
 
                     const dist = Math.hypot(tX - iX, tY - iY);
 
